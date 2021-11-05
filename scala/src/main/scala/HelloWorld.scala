bd9505b3a534a109f8e40d8290da49b0d083518e
SONG <simpledeveloper.songs@gmail.com>
2021-11-05 12:16:51 +0900

    scala-scraper test

--- scala/src/main/scala/HelloWorld.scala
+++ scala/src/main/scala/HelloWorld.scala
@@ -1,5 +1,30 @@
+import net.ruippeixotog.scalascraper.browser.JsoupBrowser
+
+import net.ruippeixotog.scalascraper.dsl.DSL._
+import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
+import net.ruippeixotog.scalascraper.dsl.DSL.Parse._
+
+import net.ruippeixotog.scalascraper.model._
+
+
 object HelloWorld {
   def main(args: Array[String]) {
-    println("Hello, world!")
+
+    val browser = JsoupBrowser()
+    val doc = browser.parseFile("example.html")
+    // Extract the text inside the element with id "header"
+    doc >> text("#header")
+
+    // Extract the <span> elements inside #menu
+    val items = doc >> elementList("#menu span")
+
+    // From each item, extract all the text inside their <a> elements
+    items.map(_ >> allText("a"))
+
+    // From the meta element with "viewport" as its attribute name, extract the
+    // text in the content attribute
+    doc >> attr("content")("meta[name=viewport]")
+
+
   }
 }
