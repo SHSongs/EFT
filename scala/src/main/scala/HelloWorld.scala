3008bcca30bd28c9c2ef026eea37470e0e3439d9
SONG <simpledeveloper.songs@gmail.com>
2021-11-05 12:40:50 +0900

    yahoo finance crawl

--- scala/src/main/scala/HelloWorld.scala
+++ scala/src/main/scala/HelloWorld.scala
@@ -7,23 +7,32 @@ import net.ruippeixotog.scalascraper.dsl.DSL.Parse._
 import net.ruippeixotog.scalascraper.model._
 
 
+import io.Source
+import java.net.URL
+
+
 object HelloWorld {
   def main(args: Array[String]) {
 
+
+    val financeURL = "https://finance.yahoo.com/quote/QQQ/history?period1=1634256000&period2=1635984000&interval=1d&filter=history&frequency=1d&includeAdjustedClose=true"
+
+    val requestProperties = Map(
+      "User-Agent" -> "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)"
+    )
+
+    val connection = new URL(financeURL).openConnection
+    requestProperties.foreach({
+      case (name, value) => connection.setRequestProperty(name, value)
+    })
+
+    val s = Source.fromInputStream(connection.getInputStream).getLines.mkString("\n")
+
     val browser = JsoupBrowser()
-    val doc = browser.parseFile("example.html")
-    // Extract the text inside the element with id "header"
-    doc >> text("#header")
+    val doc = browser.parseString(s)
 
-    // Extract the <span> elements inside #menu
-    val items = doc >> elementList("#menu span")
-
-    // From each item, extract all the text inside their <a> elements
-    items.map(_ >> allText("a"))
-
-    // From the meta element with "viewport" as its attribute name, extract the
-    // text in the content attribute
-    doc >> attr("content")("meta[name=viewport]")
+    val items = doc >> "tbody" >> "tr" >> pElementList
+    println(items)
 
 
   }
