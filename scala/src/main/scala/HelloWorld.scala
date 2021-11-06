67e9e461bbfa9a56caac39de3934a6d16ca1cff5
SONG <simpledeveloper.songs@gmail.com>
2021-11-06 13:41:55 +0900

    make requestServer func

--- scala/src/main/scala/HelloWorld.scala
+++ scala/src/main/scala/HelloWorld.scala
@@ -14,26 +14,33 @@ import java.net.URL
 object HelloWorld {
   def main(args: Array[String]) {
 
-
     val financeURL = "https://finance.yahoo.com/quote/QQQ/history?period1=1634256000&period2=1635984000&interval=1d&filter=history&frequency=1d&includeAdjustedClose=true"
 
     val requestProperties = Map(
       "User-Agent" -> "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)"
     )
 
-    val connection = new URL(financeURL).openConnection
+    val s = requestServer(financeURL, requestProperties)
+
+    val browser = JsoupBrowser()
+    val doc = browser.parseString(s)
+
+    val items = doc >> "tbody" >> "tr" >> pElementList
+
+
+  }
+
+  def requestServer(URL: String, requestProperties: Map[String, String]): String = {
+
+    val connection = new URL(URL).openConnection
     requestProperties.foreach({
       case (name, value) => connection.setRequestProperty(name, value)
     })
 
     val s = Source.fromInputStream(connection.getInputStream).getLines.mkString("\n")
 
-    val browser = JsoupBrowser()
-    val doc = browser.parseString(s)
-
-    val items = doc >> "tbody" >> "tr" >> pElementList
-    println(items)
-
-
+    s
   }
+
+
 }
