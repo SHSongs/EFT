2d9d0a54ff3d71084a9428245fbe6e048f43df95
SONG <simpledeveloper.songs@gmail.com>
2021-11-13 20:53:38 +0900

    exception handling

--- server/app/controllers/ChartController.scala
+++ server/app/controllers/ChartController.scala
@@ -1,32 +1,43 @@
 package controllers
 
+import play.api.libs.json.Json
+
 import javax.inject._
 import play.api.mvc._
-
 import utils.YahooFinanceUtil.makeYahooFinanceURL
 import utils.YahooFinanceUtil.yahooFinanceHtmlToStockData
-import utils.Util.requestServer
-import utils.Util.historicalDataToJson
+import utils.Util.{historicalDataToJson, makeJson, requestServer}
 import utils.HistoricalData
 
 
 @Singleton
 class ChartController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
   def chart(ticker: String, period1: String, period2: String) = Action {
-    val url = makeYahooFinanceURL(ticker, period1, period2)
+    var url = ""
+    var errMsg = ""
+    try {
+      url = makeYahooFinanceURL(ticker, period1, period2)
+    } catch {
+      case ex: Exception => errMsg = ex.getMessage
+    }
 
-    val requestProperties = Map(
-      "User-Agent" -> "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)"
-    )
+    if (url != "") {
+      val requestProperties = Map(
+        "User-Agent" -> "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)"
+      )
 
-    val s = requestServer(url, requestProperties)
-    val info = yahooFinanceHtmlToStockData(s)
+      val s = requestServer(url, requestProperties)
+      val info = yahooFinanceHtmlToStockData(s)
 
-    for (i <- info) println(i)
+      for (i <- info) println(i)
 
-    val historicalData = HistoricalData(ticker, period1, period2, info)
-    val json = historicalDataToJson(historicalData)
+      val historicalData = HistoricalData(ticker, period1, period2, info)
+      val json = historicalDataToJson(historicalData)
 
-    Ok(json)
+      Ok(json)
+    }
+    else {
+      Ok(makeJson("chart", errMsg))
+    }
   }
 }
