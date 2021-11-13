b652401b824dd4aff0996da2b772046ca9907c4f
SONG <simpledeveloper.songs@gmail.com>
2021-11-13 20:28:17 +0900

    ParseException handling

--- server/app/utils/Util.scala
+++ server/app/utils/Util.scala
@@ -16,11 +16,13 @@ object Util {
   def dateToUnixTime(date: String): Long = {
     // date: yyyyMMdd  ex) 20140124
     val dateString = s"$date 00:00:00 GMT"
-
     val dateFormat = new SimpleDateFormat("yyyyMMdd hh:mm:ss z")
-    val unixTime = dateFormat.parse(dateString).getTime / 1000
 
-    unixTime
+    try {
+      dateFormat.parse(dateString).getTime / 1000
+    } catch {
+      case ex: java.text.ParseException => throw new Exception("date format error!")
+    }
   }
 
   def historicalDataToJson(historicalData: HistoricalData): JsObject = {
